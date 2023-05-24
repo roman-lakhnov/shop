b6767880ffa2ad29b61e91a39a3052e065b18727
Roman Lakhnov <lakhnov.roman@gmail.com>
2023-05-24 11:00:35 +0300

    crud added query filter added static added

--- server/index.js
+++ server/index.js
@@ -3,16 +3,19 @@ const express = require('express')
 const sequelize = require('./db')
 const models = require('.//models/models')
 const cors = require('cors')
-const router=require('./routes/index')
-const errorHandler=require('./middleware/ErrorHandlerMiddleware')
-
+const fileUpload = require('express-fileupload')
+const router = require('./routes/index')
+const errorHandler = require('./middleware/ErrorHandlerMiddleware')
+const path = require('path')
 
 const PORT = process.env.PORT || 5000
 
 const app = express()
 app.use(cors())
 app.use(express.json())
-app.use('/api',router)
+app.use(express.static(path.resolve(__dirname, 'static')))
+app.use(fileUpload({}))
+app.use('/api', router)
 
 // Обработка ошибок, последний middleware
 app.use(errorHandler)
