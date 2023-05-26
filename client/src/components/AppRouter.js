8587490d4c6f24fa87cf63ff2e3a00a57ace19a5
Roman Lakhnov <lakhnov.roman@gmail.com>
2023-05-26 12:37:33 +0300

    app router added

---
+++ client/src/components/AppRouter.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Navigate, Route, Routes } from 'react-router-dom'
+import { authRoutes, publicRoutes } from '../routes'
+import { SHOP_ROUTE } from '../utils/consts'
+
+
+const AppRouter = () => {
+  const isAuth=false
+  return (
+    <Routes>
+{isAuth&&authRoutes.map(({path,element})=>
+<Route key={path} path={path} element={element}/>
+)}
+{publicRoutes.map(({path,element})=>
+<Route key={path} path={path} element={element}/>
+)}
+<Route path="*" element={<Navigate to={SHOP_ROUTE} />} />
+    </Routes>
+  )
+}
+
+export default AppRouter
---
+++ client/src/routes.js
@@ -0,0 +1,24 @@
+import Admin from './pages/Admin'
+import Auth from './pages/Auth'
+import Basket from './pages/Basket'
+import Device from './pages/Device'
+import Shop from './pages/Shop'
+import {
+	ADMIN_ROUTE,
+	BASKET_ROUTE,
+	DEVICE_ROUTE,
+	LOGIN_ROUTE,
+	REGISTRATION_ROUTE,
+	SHOP_ROUTE
+} from './utils/consts'
+
+export const authRoutes = [
+    { path: ADMIN_ROUTE, element: <Admin /> },
+    { path: BASKET_ROUTE, element: <Basket /> }
+]
+export const publicRoutes = [
+	{ path: REGISTRATION_ROUTE, element: <Auth /> },
+	{ path: LOGIN_ROUTE, element: <Auth /> },
+	{ path: SHOP_ROUTE, element: <Shop /> },
+	{ path: DEVICE_ROUTE + '/:id', element: <Device /> },
+]
